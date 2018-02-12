640c52b236584f96cc609458874516483da64dc9
Angel <angellecuona84@gmail.com>
2018-02-12 17:22:17 -0600

    First Commit

---
+++ src/main/java/com/whirlpool/whirlpoolBackend/WhirlpoolBackendApplication.java
@@ -0,0 +1,12 @@
+package com.whirlpool.whirlpoolBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhirlpoolBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhirlpoolBackendApplication.class, args);
+	}
+}
