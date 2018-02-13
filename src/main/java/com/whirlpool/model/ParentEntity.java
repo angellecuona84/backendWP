2ebe2843f85c2958c8d0329fc05af71f1dd0eaf9
Angel <angellecuona84@gmail.com>
2018-02-13 13:32:35 -0600

    Add Model Class

---
+++ src/main/java/com/whirlpool/model/ParentEntity.java
@@ -0,0 +1,23 @@
+package com.whirlpool.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@MappedSuperclass
+@Access(AccessType.FIELD)
+public class ParentEntity implements Serializable{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", unique = true, nullable = false)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+}
---
+++ src/main/java/com/whirlpool/model/Project.java
@@ -0,0 +1,31 @@
+package com.whirlpool.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "project")
+@Access(AccessType.FIELD)
+public class Project extends ParentEntity {
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
